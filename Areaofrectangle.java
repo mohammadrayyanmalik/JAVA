758316c17f6bf7ffc5d46e8ab9b164e0b37f1779
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-08-29 09:03:05 +0530

    additionassignment

---
+++ Areaofrectangle.java
@@ -0,0 +1,21 @@
+class Areaofrectangle
+{
+	
+	public static void main(String []args)
+	{
+		// Areaof rectangle
+		//rectangle formula is lenght*bredth
+		int l=10;
+		int b=20;
+		System.out.println("Area of ractangle is "+(l*b));
+		
+		
+		
+		// area of square 
+		// area of sqare ka formula is side*side
+		int side=10;
+		System.out.println("arae of square is "+(side*side));
+		
+	}
+	
+}
---
+++ p1.java
@@ -0,0 +1,39 @@
+class p1{
+	public static void main(String []args)
+	{
+		
+		//unary operator it's deals with only one operand always
+		// ++ increament    -- decrement
+		
+		
+		// increament
+		int i=10;
+		i++;
+		System.out.println(i);
+		
+		
+		//decreament
+		int j=10;
+		j--;
+		j--;
+		i--;
+		j--;
+		j++;
+		i++;
+		System.out.println(j+i);
+		
+		// if write ++ before variable it means pre inreament
+		// if write ++ after variable it means post inreament
+		// if write -- before variable it means pre decrement
+		// if write -- after variable it means pro decrement
+		
+		System.out.println(++i);
+		System.out.println((++j)+(++i));
+		System.out.println((++j)+(++i));
+		
+		
+		
+	}
+	
+	
+}
