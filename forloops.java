dcedfd5f865d71fa785fac95cecc0f56fb903faa
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-10 08:29:10 +0530

    sum in for loops

--- forloops.java
+++ forloops.java
@@ -8,15 +8,36 @@ class forloops
 	public static void main(String[]args)
 	{
 		Scanner sc=new Scanner(System.in);
+		
+		/*System.out.println("13 to 21");
+		
 		for(int i=sc.nextInt(); i<=21; i++)
 		{
 			System.out.print(i+" ");
-			
 		}
+		
+		System.out.println("21 to 13");
+		
 		for(int i=sc.nextInt(); i>=13; i--)
 		{
-			System.out.print(i+" ");
-			
+			System.out.print(i+" ");	
+		}*/
+		
+		
+		// to print table of 5
+		/*int  n=sc.nextInt();
+		for (int i=1; i<=10; i++)
+		{
+			System.out.println(n*i);
+		}*/
+		
+		// sum of no upto n
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1; i<=n; i++)
+		{
+			sum=sum+i;	
 		}
+		System.out.println(sum);
 	}
 }
