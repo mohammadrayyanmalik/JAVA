8b94a93ba99b04cfaa60544ea68301dfb91a95c3
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-16 07:59:48 +0530

    typecasting 1

---
+++ p30.java
@@ -0,0 +1,26 @@
+class p30
+{						
+	public static void main(String[]args)
+	{
+		for (int i=65; i<=90; i++)
+		{
+			if (i==89)
+			{
+			System.out.println((char)i);
+			}
+		}
+		
+			System.out.println("================================");
+		
+		
+		// small a to z 
+		
+		for (int i=97; i<=97+25;  i++)
+		{
+			
+			System.out.println((char)i);
+			
+		}
+		
+	}
+}
--- typecasting.java
+++ typecasting.java
@@ -21,5 +21,45 @@ class typecasting
 	System.out.println(d);
 	
 	
+	
+	System.out.println("==========================================================================");
+	
+	
+	// int to char
+	
+	int n=65;
+	char ch=(char)n;
+	System.out.println(ch);
+	
+	System.out.println((char)n);
+	
+			System.out.println("================================");
+	
+	
+	for (int i=65; i<=90; i++)
+		{
+			if (i==89)
+			{
+			System.out.println((char)i);
+			}
+		}
+		
+			System.out.println("================================");
+		
+		
+		// small a to z 
+		
+		for (int i=97; i<=97+25;  i++)
+		{
+			
+			System.out.println((char)i);
+			
+		}
+	
+	
+	
+	
+	
+	
 	}
 }
