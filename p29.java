50b2f84a36dd6fde842146b985502d307f32a720
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-19 08:25:57 +0530

    classes and objects

---
+++ p29.java
@@ -0,0 +1,49 @@
+class Employee
+{	// instance variable
+	int id;
+	String name;
+	String add;
+	String email;
+}
+
+class p29
+{
+	public static void main(String[]args)
+	{
+	Employee e1=new Employee();
+	Employee e2=new Employee();
+	Employee e3=new Employee();
+	
+	e1.id=1;
+	e1.name="Rayyan";
+	e1.add="xzy";
+	e1.email="mrr@gmail.com";
+	
+	System.out.println(e1.id);
+	System.out.println(e1.name);
+	System.out.println(e1.add);
+	System.out.println(e1.email);
+	
+	System.out.println("================================");
+	
+	e2.id=2;
+	e2.name="Asrar";
+	e2.add="abc";
+	
+	System.out.println(e2.id);
+	System.out.println(e2.name);
+	System.out.println(e2.add);
+	
+	System.out.println("================================");
+	
+	e3.id=3;
+	e3.name="Amit";
+	e3.add="qrf";
+	
+	System.out.println(e3.id);
+	System.out.println(e3.name);
+	System.out.println(e3.add);
+	
+	
+	}
+}
