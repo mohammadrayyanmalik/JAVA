0eec87f6ce8fe50a7abcc5046ae73f1de1f6531b
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-20 08:03:54 +0530

    claculator method

---
+++ method2.java
@@ -0,0 +1,51 @@
+class Calculator
+{
+	int a;
+	int b;
+	
+	int addition()
+	{
+		return a+b;
+	}
+	
+	
+	int substraction()
+	{
+		return a-b;
+	}
+	
+	
+	void setValues(int n1, int n2)
+	{
+		a=n1;
+		b=n2;
+	}
+	
+}
+
+
+
+
+class method2
+{
+	public static void main(String []args)
+	{
+		
+		Calculator c1=new Calculator();
+		c1.a=12;
+		c1.b=10;
+		
+		int sum=c1.addition();
+		
+		System.out.println("Addition  is "+ sum);
+		
+		int sub=c1.substraction();
+		System.out.println("substraction  is "+ sub);
+	
+		Calculator c2=new Calculator();
+		c2.setValues(122,2);
+		System.out.println(c2.a+" "+c2.b);
+		
+		
+	}
+}
