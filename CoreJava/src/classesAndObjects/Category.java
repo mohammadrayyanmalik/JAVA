e69d6ef780b8b66388c5118d02fdf2b2488c0424
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-24 07:38:48 +0530

    constructor

---
+++ CoreJava/src/classesAndObjects/Category.java
@@ -0,0 +1,17 @@
+package classesAndObjects;
+
+public class Category {
+	int categoryId;
+	String categoryName;
+	
+	
+	//with this it is default constructor
+	Category()
+	{
+		System.out.println("Category object created");
+	}
+	
+	
+}
+
+
---
+++ CoreJava/src/classesAndObjects/Category1.java
@@ -0,0 +1,15 @@
+package classesAndObjects;
+
+public class Category1 {
+	public static void main(String[] args) {
+	//		constructor it's a default cunstroctor
+		new Category();
+		new Category();
+		new Category();
+	}
+}
+
+
+
+// constructor name always same as class name
+// constructer will be call whenever object  will be created
