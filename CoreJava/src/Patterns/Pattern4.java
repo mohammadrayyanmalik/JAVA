4eca4b717cf5c16a3e2a019d32ee1d6494b8507c
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-28 08:31:24 +0530

    triangle pattern

---
+++ CoreJava/src/Patterns/Pattern4.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class Pattern4 {
+	
+	public static void main(String[] args) {
+		
+		int n=100;
+		for(int i=1; i<=n;i++)
+		{
+			
+			for (int j=1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+}
