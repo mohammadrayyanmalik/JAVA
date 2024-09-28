88b3bf8b860ca62512f3d32bd7bdad2927c33f62
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-28 07:44:14 +0530

    parrerns

---
+++ CoreJava/src/Patterns/Pattern1.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		
+		for(int j=1; j<=2; j++)// rows
+		{
+			
+			for (int i=1; i<=2; i++)// colum
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ CoreJava/src/Patterns/pattern2.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class pattern2 {
+	public static void main(String[] args) {
+		for (int i=1; i<=10;i++)
+		{
+			for (int j=1;j<=5;j++)
+			{
+				//System.out.print(i);
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
