45af8cee9a67cbc03891d9a18fc76f75cc7a9131
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-10-08 09:00:17 +0530

    String

---
+++ CoreJava/src/StringExamples/Example7.java
@@ -0,0 +1,10 @@
+package StringExamples;
+
+public class Example7 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello".substring(1));
+		System.out.println("Hello".substring(0, 2));
+		
+	}
+}
