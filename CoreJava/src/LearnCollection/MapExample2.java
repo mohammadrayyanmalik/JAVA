16899d1dc2735c5f9efe1f66ab95232dd9b21feb
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-10-24 07:56:50 +0530

    LinkedHashMap

---
+++ CoreJava/src/LearnCollection/MapExample2.java
@@ -0,0 +1,53 @@
+package LearnCollection;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class MapExample2 {
+		
+	public static void main(String[] args) {
+		
+		LinkedHashMap<String , String> phoneNumber=new LinkedHashMap<String, String>();
+		
+		phoneNumber.put("amit", "111");
+		phoneNumber.put("samir", "333");
+		phoneNumber.put("asrar", "222");
+		phoneNumber.put("Rayyan", "111");
+		
+		System.out.println(phoneNumber);
+		
+		System.out.println("==================================================================");
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String name=sc.next();
+		
+		if (phoneNumber.containsKey(name))
+		{
+			System.out.println("Phone no is present "+phoneNumber.get(name));
+			System.out.println("Do you want to change it (Y/N)");
+			char desicion=sc.next().charAt(0);
+			if (desicion=='Y')
+			{
+				String newPhoneNO=sc.next();
+				phoneNumber.put(name, newPhoneNO);
+			}
+			else
+			{
+				System.out.println("Invalid Keys");
+			}
+			
+		}
+		
+		else {
+			String phoneNo=sc.next();
+			phoneNumber.put(name, phoneNo);
+		}
+		
+		
+		System.out.println(phoneNumber);
+	}
+	
+}
+// Hashmap does not maintain insertion order while Linked hashMap maintain insertion order
+// hashmap and LinkedHashMap has both are similar properties (key should be unique and value can be duplicate)
---
+++ CoreJava/src/LearnCollection/MapExample3.java
@@ -0,0 +1,5 @@
+package LearnCollection;
+
+public class MapExample3 {
+
+}
