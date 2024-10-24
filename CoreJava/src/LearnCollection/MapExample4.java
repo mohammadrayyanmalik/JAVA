21be957775c9f856c2eed4a95ea0b00fdf590762
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-10-24 08:55:13 +0530

    last collection or map

--- CoreJava/src/LearnCollection/MapExample4.java
+++ CoreJava/src/LearnCollection/MapExample4.java
@@ -42,7 +42,40 @@ public class MapExample4 {
 		System.out.println(map.floorKey(110));
 		System.out.println(map.floorEntry(20));
 		
+		System.out.println("======================================================");
+		
+		System.out.println(map.headMap(201));
+		System.out.println(map.headMap(201, true));
+		
+		
+		System.out.println("======================================================");
+		
+		System.out.println(map.tailMap(106));
+		System.out.println(map.tailMap(106,false));
+		
+		
+		System.out.println("======================================================");
+
+	System.out.println(map.subMap(102, 222));
+	System.out.println(map.subMap(102, true,119,true));
+	
+	System.out.println("======================================================");
+		System.out.println(map.firstKey()); // first key 
+		System.out.println(map.lastKey());
+		System.out.println(map.firstEntry());
+		System.out.println(map.lastEntry());
+	
+		
+		System.out.println("================================================");
+			System.out.println(map.pollFirstEntry());
+			System.out.println(map);
+			
+			System.out.println(map.pollLastEntry());
+			System.out.println(map);
+	
 		
 	}
 }
 
+// headmap ka return type map hoga
+//it show range but first value by default include and last is exclusive
