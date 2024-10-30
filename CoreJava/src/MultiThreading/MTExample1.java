86c7f351ba0e11d970e106600629188e67fccb93
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-10-30 08:10:53 +0530

    multitherading

--- CoreJava/src/MultiThreading/MTExample1.java
+++ CoreJava/src/MultiThreading/MTExample1.java
@@ -25,6 +25,12 @@ class T2 extends Thread
 		for (int i=1;i<=10; i++) 
 		{
 			System.out.println("By By");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	
 	}
@@ -34,11 +40,25 @@ public class MTExample1 {
 
 	public static void main(String[] args) {
 
+		
+//		System.out.println("Main Methods");
 			T1 t1=new T1();
 			t1.start();
 		
 			T2 t2=new T2();
 			t2.start();
+			
+			try {
+				t1.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}// jab tak t2 apna kam finish  nahikarega tab tak main method execute nahi hoga
+			
+			
+			
+			System.out.println("main methods");
+			
 			
 			
 	}
@@ -53,3 +73,4 @@ public class MTExample1 {
 // thread 1 time karne ke baad woh dead ho jati hai
 // thread has a capability to stop your programm for some time
 // for gap of 1 second write in sleep method 1000   eg.sleep(1000)
+// main method is also a thread 
