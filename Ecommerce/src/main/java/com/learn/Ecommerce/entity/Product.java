335b29e0f16f068a3c572d97199b8f5a4f9781a1
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-11-14 09:00:16 +0530

    project of springboot and connectivity of database

---
+++ Ecommerce/src/main/java/com/learn/Ecommerce/entity/Product.java
@@ -0,0 +1,33 @@
+package com.learn.Ecommerce.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+//@Table(name="product_detials") will help to change table name
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+
+public class Product {
+
+	@Id
+	private int productId;
+	@Column(nullable =false)
+	private String productName;
+	@Column(nullable =false)
+	private String description;
+	@Column(nullable=false)
+	private int productPrice;
+
+	
+	
+	
+}
