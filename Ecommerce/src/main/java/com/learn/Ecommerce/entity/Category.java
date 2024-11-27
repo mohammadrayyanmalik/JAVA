45c0826359af3a43a3c8c1069e367a192999e31f
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-11-27 09:04:56 +0530

    spring boot project2

---
+++ Ecommerce/src/main/java/com/learn/Ecommerce/entity/Category.java
@@ -0,0 +1,9 @@
+package com.learn.Ecommerce.entity;
+
+public class Category {
+
+	private String categoryId;
+	private String categoryName;
+	private String categoryDesc;
+	
+}
---
+++ Ecommerce/src/main/java/com/learn/Ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.learn.Ecommerce.repository;
+
+import java.util.Locale.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, String>{
+
+}
